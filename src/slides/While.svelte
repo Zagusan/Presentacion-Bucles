2e008654e8fd3041e9b0cf4fd2c2042d26203578
Zagusan <santiagopina@protonmail.com>
2026-03-15 13:34:48 -0600

    Reorganize `while` examples to enhance clarity

--- src/slides/While.svelte
+++ src/slides/While.svelte
@@ -31,6 +31,7 @@
             <h3>Cómo crear un bucle <code>while</code></h3>
             <StepwiseCode code="while True:">
                 <p>Para crear un bucle infinito, se puede colocar <code><b>True</b></code></p>
+                <p>Después de todo <code><b>True == True</b></code> siempre es verdadero</p>
             </StepwiseCode>
         </Background>
     </section>
@@ -38,36 +39,9 @@
         <Background blur tint="rgba(0, 0, 0, 0.4)">
             <h3>Cómo crear un bucle <code>while</code></h3>
             <StepwiseCode 
-            code="numero = 0
-        
-        while numero < 10:">
-                <p>En este ejemplo, vamos a comprobar que el número sea menor a 10</p>
-            </StepwiseCode>
-        </Background>
-    </section>
-    <section data-auto-animate data-transition="fade">
-        <Background blur tint="rgba(0, 0, 0, 0.4)">
-            <h3>Cómo crear un bucle <code>while</code></h3>
-            <StepwiseCode 
-            code="numero = 0
-        
-        while numero < 10:
+            code="while True:
         →→→→pass">
                 <p>Se añade la indentación con <b>tab</b> o poniendo 4 espacios</p>
-            </StepwiseCode>
-        </Background>
-    </section>
-    <section data-auto-animate data-transition="fade">
-        <Background blur tint="rgba(0, 0, 0, 0.4)">
-            <h3>Cómo crear un bucle <code>while</code></h3>
-            <StepwiseCode 
-            code="numero = 0
-        
-        while numero < 10:
-            # Imprime los números del 0 al 9
-            print(numero)
-            numero += 1">
-                <p>Se coloca dentro del bucle lo que se quiere repetir</p>
             </StepwiseCode>
         </Background>
     </section>
@@ -79,7 +53,43 @@
             # Recibe un nombre y dice hola
             nombre = input({"\"Nombre: \""})
             print(f{"\"¡Hola {nombre}!\""})">
-                <p>Este ejemplo usa un bucle infinito para nunca detenerse</p>
+                <p>Se coloca dentro del bucle lo que se quiere repetir</p>
+            </StepwiseCode>
+        </Background>
+    </section>
+    <section data-auto-animate data-transition="fade">
+        <Background blur tint="rgba(0, 0, 0, 0.4)">
+            <h3>Cómo crear un bucle <code>while</code></h3>
+            <StepwiseCode 
+            code="numero = 0
+        
+        while numero < 10:">
+                <p>En este ejemplo, vamos a imprimir los números del 0 al 9 al comprobar que <code>numero</code> sea menor que 10</p>
+            </StepwiseCode>
+        </Background>
+    </section>
+    <section data-auto-animate data-transition="fade">
+        <Background blur tint="rgba(0, 0, 0, 0.4)">
+            <h3>Cómo crear un bucle <code>while</code></h3>
+            <StepwiseCode 
+            code="numero = 0
+        
+        while numero < 10:
+            print(numero)">
+                <p>Se coloca dentro del bucle el <code><b>print()</b></code></p>
+            </StepwiseCode>
+        </Background>
+    </section>
+    <section data-auto-animate data-transition="fade">
+        <Background blur tint="rgba(0, 0, 0, 0.4)">
+            <h3>Cómo crear un bucle <code>while</code></h3>
+            <StepwiseCode 
+            code="numero = 0
+        
+        while numero < 10:
+            print(numero)
+            numero += 1">
+                <p>Es <b>muy importante</b> colocar <code>numero += 1</code>. De lo contrario, el número siempre será menor a 10, creando un bucle infinito</p>
             </StepwiseCode>
         </Background>
     </section>
